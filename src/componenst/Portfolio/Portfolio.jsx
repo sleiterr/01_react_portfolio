b1ba23060b67d0e66e9b73f3b065640fed86560c
Oleg <sleite4r@gmail.com>
2025-05-11 19:15:23 +0200

    update DataJSON, style overlay, add new fornt-family

--- src/componenst/Portfolio/Portfolio.jsx
+++ src/componenst/Portfolio/Portfolio.jsx
@@ -3,28 +3,30 @@ import s from "./Portfolio.module.css";
 // import s from "./Portfolio.module.css";
 
 const Portfolio = ({ type }) => {
-  const categoryProjects = type.filter(
-    (item) => item.category === "js-project"
+  const categoryProjects = type.filter((item) =>
+    ["js-project", "react-project"].includes(item.category)
   );
   console.log("Filtered projects:", categoryProjects);
   return (
     <section>
       <div className={`${s.galleryContainer} container`}>
-        {categoryProjects.map((item, index) => {
-          const formattedIndex = (index + 1).toString().padStart(2, "0");
-          return (
-            <div key={item.id} className={s.galleryItem}>
-              <div className={s.galleryContent}>
-                <img src={item.src} alt={item.caption} className={s.img} />
-                <a href={item.url} className={s.overlay} target="_blank">
-                  <p className={s.cardNumber}>${formattedIndex}</p>
-                  <h3 className={`${s.cardTitle} ${s.head}`}>{item.title}</h3>
-                </a>
+        <div className={s.gallery}>
+          {categoryProjects.map((item, index) => {
+            const formattedIndex = (index + 1).toString().padStart(2, "0");
+            return (
+              <div key={item.id} className={s.galleryItem}>
+                <div className={s.galleryContent}>
+                  <img src={item.src} alt={item.caption} className={s.img} />
+                  <a href={item.url} className={s.overlay} target="_blank">
+                    <p className={s.cardNumber}>{formattedIndex}</p>
+                    <h3 className={`${s.cardTitle} ${s.head}`}>{item.title}</h3>
+                  </a>
+                </div>
+                <p className={s.imgCaption}>{item.caption}</p>
               </div>
-              <p className={s.imgCaption}>{item.caption}</p>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </section>
   );

26df87de755c17f627870fa1a15d685824148afa
Oleg <sleite4r@gmail.com>
2025-07-07 14:58:08 +0200

    implement responsive hero section and add custom breakpoints

--- src/componenst/Hero/HeroSection.jsx
+++ src/componenst/Hero/HeroSection.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import s from "./HeroSection.module.css";
-// import heroBg from "../../assets/img/hero-img.jpg";
+import heroBg from "../../assets/img/hero-design---Copy-1.jpg";
 
 const HeroSection = () => {
   return (
     <section
-      className={s.sectionHero}
+      className="relative w-full bg-cover bg-no-repeat bg-center "
       id="#"
-      // style={{ backgroundImage: `url(${heroBg})` }}
+      style={{
+        backgroundImage: `url(${heroBg})`,
+        backgroundPosition: "center center",
+        aspectRatio: "1920 / 1200",
+      }}
     >
       <div className={`${s.heroConteiner} container`}>
         <div className={s.heroContent}>
           <h1 className={s.heroTitle}>Hi, I'm Oleg.</h1>
           <h3 className={s.heroSubtitle}>
-            I am a <span className="text-heading-accent">Web Developer</span>
+            I am <span className="text-heading-accent">Web Developer</span>
           </h3>
           <p className={s.heroContext}>
             I am currently studying web development at Media College Denmark,

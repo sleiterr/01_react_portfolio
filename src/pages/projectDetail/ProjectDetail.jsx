480e8cbac2ace4be556a83f2d3dcd4ebcabfc1d9
Oleg <sleite4r@gmail.com>
2025-07-30 21:06:10 +0200

    refactor width context_container, refactor icons

--- src/pages/projectDetail/ProjectDetail.jsx
+++ src/pages/projectDetail/ProjectDetail.jsx
@@ -37,15 +37,15 @@ const ProjectDetail = () => {
   return (
     <section className="pt-16">
       <div className="py-[120px] px-50 mx-auto max-w-5x1">
-        <div className="grid grid-cols-2 items-center justify-center gap-[6rem]">
+        <div className="grid grid-cols-2 items-center justify-center gap-4">
           <div className="flex items-center justify-center">
             <img
               src={project.preview}
               alt="project image"
-              className="w-full h-auto"
+              className="w-[560px] h-auto"
             />
           </div>
-          <div className="flex flex-col items-start justify-center">
+          <div className="flex flex-col items-start justify-center w-[590px]">
             <div className="flex flex-col gap-4 mb-[40px]">
               <h2 className="font-code font-normal text-heading-primary text-5xl tracking-wide ">
                 {project.title}
@@ -55,8 +55,8 @@ const ProjectDetail = () => {
               </p>
             </div>
 
-            <ul className="relative after:content[''] after:absolute after:top-0 after:left-0 after:w-full after:h-[1px] after:bg-white before:content[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-[1px] before:bg-white flex flex-col gap-2 mb-10">
-              <li className=" flex justify-between  w-full py-4">
+            <ul className="relative w-full after:content[''] after:absolute after:top-0 after:left-0 after:w-full after:h-[1px] after:bg-white before:content[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-[1px] before:bg-white flex flex-col gap-2 mb-10">
+              <li className=" flex justify-between w-full py-4">
                 <p className="font-light text-xl text-content tracking-wide">
                   Year:
                 </p>
@@ -64,22 +64,30 @@ const ProjectDetail = () => {
                   {project.date}
                 </p>
               </li>
-              <li className="relative after:content[''] after:absolute after:top-0 after:left-0 after:w-full after:h-[1px] after:bg-white flex justify-between w-full py-4">
+              <li className="relative h-full after:content[''] after:absolute after:top-0 after:left-0 after:w-full after:h-[1px] after:bg-white flex justify-between py-4">
                 <p className="font-light text-xl text-content tracking-wide">
                   Role:
                 </p>
-                <p className="font-light text-xl text-content tracking-wide w-[50%]">
+                <p className="font-light text-xl text-content tracking-wide  text-right w-[320px] leading-8">
                   {project.role}
                 </p>
               </li>
             </ul>
-            <div className="flex justify-between w-[60%]">
-              <p className="flex items-center gap-4 font-light text-xl text-content tracking-wide">
-                Live Page <IoIosArrowRoundForward />
-              </p>
-              <p className="flex items-center gap-4 font-light text-xl text-content tracking-wide">
-                View GitHub <FaGithub />
-              </p>
+            <div className="flex justify-between w-full">
+              <a
+                href={project.url}
+                target="_blank"
+                className="flex items-center gap-4 font-light text-xl text-content tracking-wide"
+              >
+                Live Page <IoIosArrowRoundForward className="w-[48px] h-full" />
+              </a>
+              <a
+                href={project.github}
+                target="_blank"
+                className="flex items-center gap-4 font-light text-xl text-content tracking-wide"
+              >
+                View GitHub <FaGithub className="w-[32px] h-full" />
+              </a>
             </div>
           </div>
         </div>

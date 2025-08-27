0961828d0696cb5963833c50469b1a26ccfebedc
Oleg <sleite4r@gmail.com>
2025-08-27 11:50:36 +0200

    Adjusted padding and repositioned some elements

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -21,10 +21,10 @@ const Skills = () => {
         <h2 className="font-code font-normal text-5xl text-heading-primary tracking-wide mb-15">
           My Skills
         </h2>
-        <div className="">
+        <div className="relative min-h-[26rem]">
           {/* Frontend Stack */}
-          <ul className="px-8 pt-4 pb-16 w-[31.2rem] h-auto rounded-xl bg-white/20 backdrop-blur-sm">
-            <li className="">
+          <div className="absolute translate-x-1/2 px-8 pt-4 pb-16 w-[31.2rem] h-auto rounded-xl bg-white/20 backdrop-blur-sm">
+            <div className="">
               <h4 className="font-light text-content text-base tracking-wider mb-4">
                 Frontend Stack
               </h4>
@@ -35,12 +35,12 @@ const Skills = () => {
                 <img src={html} alt="html" className="size-14" />
                 <img src={css} alt="css" className="size-14" />
               </div>
-            </li>
-          </ul>
+            </div>
+          </div>
 
           {/* CMS & Data Layer */}
-          <ul className=" flex flex-col pt-4 pl-6 pb-8 w-[24.6rem] h-[9.2rem] rounded-xl  bg-white/20 backdrop-blur-sm z-1">
-            <li className="">
+          <div className="absolute translate-y-32 left-[30rem] pt-4 pl-6 pb-8 w-[24.6rem] h-[9.2rem] rounded-xl  bg-white/20 backdrop-blur-sm z-1">
+            <div className="">
               <h4 className="font-light text-content text-base tracking-wider mb-4">
                 CMS & Data Layer
               </h4>
@@ -52,12 +52,12 @@ const Skills = () => {
                   className="w-[2.5rem] h-auto"
                 />
               </div>
-            </li>
-          </ul>
+            </div>
+          </div>
 
           {/* Development Workflow */}
-          <ul className="pt-4 px-6 pb-8  w-[23.4rem] h-[8.1rem] rounded-xl bg-white/20 backdrop-blur-sm z-0 top-[15rem] translate-x-[12rem]">
-            <li className="">
+          <div className="absolute pt-4 px-6 pb-8  w-[23.4rem] h-[8.1rem] rounded-xl bg-white/20 backdrop-blur-sm z-0 top-[15rem] translate-x-[12rem]">
+            <div className="">
               <h4 className="font-light text-content text-base tracking-wider mb-4">
                 Development Workflow
               </h4>
@@ -65,12 +65,12 @@ const Skills = () => {
                 <img src={gitHub} alt="Git Hub" className="size-12" />
                 <img src={vercel} alt="vercel" className="w-[7rem] h-auto" />
               </div>
-            </li>
-          </ul>
+            </div>
+          </div>
 
           {/* UI/UX Tools */}
-          <ul className="px-6 pt-4 pb-8 w-[23rem] h-[8rem] bg-white/20 backdrop-blur-sm z-2 rounded-xl">
-            <li className="absolute">
+          <div className="absolute translate-y-56 right-[11rem] px-6 pt-4 pb-8 w-[23rem] h-[8rem] bg-white/20 backdrop-blur-sm z-2 rounded-xl">
+            <div className="absolute">
               <h4 className="font-light text-content text-base tracking-wider mb-2">
                 UI/UX Tools
               </h4>
@@ -79,8 +79,8 @@ const Skills = () => {
                 <img src={ph} alt="photoShop" className="size-12" />
                 <img src={ai} alt="adobe Illustrator" className="size-12" />
               </div>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </div>
     </section>

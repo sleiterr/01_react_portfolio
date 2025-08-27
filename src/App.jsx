564b129437a40b33710ef9fac748db7732611313
Oleg <sleite4r@gmail.com>
2025-08-27 21:29:12 +0200

    add hover element, hover color to navigation link

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./pages/home/Home";
 
-import ProjectDetail from "./pages/projectDetail/ProjectDetail";
+import ProjectDetail from "./pages/projectDetail/project-detail";
 
 function App() {
   return (

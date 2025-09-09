343b751b206169e996a0abce6d85088b6231a138
Oleg <sleite4r@gmail.com>
2025-09-09 18:53:09 +0200

    refactor: move tech logos to separate data file and use LogoLoop component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,14 @@ import About from "../../components/About/About";
 import Skills from "../../components/Skills/Skills";
 import Experience from "../../components/Experience/Experience";
 import Portfolio from "../../components/Portfolio/Portfolio";
+import Skillslop from "../../components/Skillslop/SkillTicker";
+import { techLogos } from "../../Data/techLogos";
+// import {
+//   SiReact,
+//   SiNextdotjs,
+//   SiTypescript,
+//   SiTailwindcss,
+// } from "react-icons/si";
 
 const Home = () => {
   const heroRef = React.useRef(null);
@@ -11,6 +19,24 @@ const Home = () => {
     <>
       <main>
         <HeroSection heroRef={heroRef} />
+        <div
+          className="block md:hidden"
+          style={{ height: "100px", position: "relative", overflow: "hidden" }}
+        >
+          <Skillslop
+            logos={techLogos}
+            speed={90}
+            direction="left"
+            logoHeight={40} //heght_icon
+            gap={40} //width_icon
+            // className="px-12"
+            pauseOnHover
+            scaleOnHover
+            fadeOut
+            fadeOutColor="#ffffff"
+            ariaLabel="Technology partners"
+          />
+        </div>
         <About />
         <Skills />
         <Portfolio />

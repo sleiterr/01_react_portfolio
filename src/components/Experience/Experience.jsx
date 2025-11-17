e47c1010641d528975f2e3036abf3480ead950cc
Oleg <sleite4r@gmail.com>
2025-11-17 09:02:54 +0100

    refactore styling experience.jsx, pagination

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -6,25 +6,52 @@ import AccordionPath from "./AccordionPath";
 import AccordionStudy from "./AccordionStudy";
 import experienceData from "../../Data/experienceData.json";
 import educationData from "../../Data/educationData.json";
+import clsx from "clsx";
 
 const Experience = () => {
   return (
     <section className="" id="experience">
       <div className="px-4 py-[3.7rem] md:py-[8rem] mx-auto md:max-w-7xl">
-        <div className="grid cols-1 gap-4 md:gap-8 lg:grid-cols-2 lg:gap-12 items-start">
+        <div
+          className={clsx(
+            "grid cols-1 gap-4",
+            "lg:grid-cols-2 lg:gap-12 lg:place-items-start",
+            "md:place-items-center md:gap-8"
+          )}
+        >
           <div className="">
-            <h2 className="flex items-center justify-start gap-4 font-code text-heading-primary text-4xl md:text-5xl tracking-wide mb-4 md:mb-10">
-              <div className="relative w-22 h-22 flex items-center justify-center border-2 border-white rounded-full">
-                <GiGraduateCap className="w-12 h-12 text-white" />
+            <h2
+              className={clsx(
+                "flex items-center justify-start gap-4 mb-4 md:mb-10",
+                "font-code text-heading-primary text-4xl md:text-5xl tracking-wide"
+              )}
+            >
+              <div
+                className={clsx(
+                  "relative flex items-center justify-center border-2 border-white rounded-full",
+                  "lg:w-22 lg:h-22 md:w-18 md:h-18"
+                )}
+              >
+                <GiGraduateCap className="lg:w-14 lg:h-14 md:w-10 md:h-10 text-white" />
               </div>
               Education
             </h2>
             <AccordionStudy educationData={educationData} />
           </div>
           <div className="">
-            <h2 className="flex items-center justify-start gap-4 font-code text-heading-primary text-4xl md:text-5xl tracking-wide mb-4 md:mb-10">
-              <div className="relative w-22 h-22 flex items-center justify-center border-2 border-white rounded-full">
-                <FaAward className="w-12 h-12 text-white" />
+            <h2
+              className={clsx(
+                "flex items-center justify-start gap-4 mb-4 md:mb-10",
+                "font-code text-heading-primary text-4xl md:text-5xl tracking-wide"
+              )}
+            >
+              <div
+                className={clsx(
+                  "relative flex items-center justify-center border-2 border-white rounded-full",
+                  "lg:w-22 lg:h-22 md:w-18 md:h-18"
+                )}
+              >
+                <FaAward className="lg:w-12 lg:h-12 md:w-10 md:h-10 text-white" />
               </div>
               Experience
             </h2>
--- src/components/Hero/ButtHero.jsx
+++ src/components/Hero/ButtHero.jsx
@@ -6,10 +6,12 @@ export const ButtHero = () => {
     <div>
       <button
         className={clsx(
-          "font-medium text-lg tracking-wide rounded-md text-content py-3 px-12 bg-button-gradient mt-8",
+          "font-light text-lg tracking-wide rounded-md text-content py-3 px-12 bg-button-gradient mt-8",
           "md:py-4 md:px-12 md:text-2xl",
           " lg:mt-12 lg:text-xl lg:py-4 lg:px-12",
-          "cursor-pointer focus:outline-2 focus:outline-offset-2 focus:outline-slate-600 active:bg-slate-600"
+          "cursor-pointer focus:outline-3 focus:outline-offset-2 focus:outline-slate-600 active:bg-slate-600",
+          "transition-all duration-300 ease-in-out",
+          "hover:brightness-110"
         )}
       >
         Download Resume

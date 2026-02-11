2d78f04bfdac1b4fa659150458757c3502e67c23
Oleg Troian <sleite4r@gmail.com>
2026-02-11 10:00:01 +0100

    refactor: enable direct CV download instead of opening in new tab

--- src/utils/gtag.js
+++ src/utils/gtag.js
@@ -10,18 +10,22 @@ export function loadGoogleAnalytics() {
   // set the source to the gtag.js with our GA_ID
   gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
 
+  // function to initialize gtag after the script is loaded
   gtagScript.onload = () => {
+    // window.dataLayer is used by gtag.js
     window.dataLayer = window.dataLayer || [];
 
+    // function to push events to dataLayer
     function gtag() {
       dataLayer.push(arguments);
     }
 
+    // expose gtag function to the global window object
     window.gtag = gtag;
     gtag("js", new Date());
     gtag("config", GA_ID);
   };
-
+    
   // append both scripts to head
   document.head.appendChild(gtagScript);
 }
